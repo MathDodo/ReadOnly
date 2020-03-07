4345841cb46a7a83a9c9cd3ad0a2ccb0da89f26d
MathDodo <mathias.h.larsen@jp.dk>
2020-03-08 00:45:42 +0100

    ReadOnly attribute

---
+++ Assets/FoldergeistAssets/Demo/ReadOnly/ReadOnlyDemo.cs
@@ -0,0 +1,11 @@
+﻿using FoldergeistAssets.ReadOnly;
+using UnityEngine;
+
+public class ReadOnlyDemo : MonoBehaviour
+{
+    [SerializeField]
+    private int _test1;
+
+    [SerializeField, ReadOnly]
+    private int _test2 = 445;
+}
---
+++ Assets/FoldergeistAssets/ReadOnly/Scripts/Other/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace FoldergeistAssets
+{
+    namespace ReadOnly
+    {
+        public class ReadOnlyAttribute : PropertyAttribute
+        {
+        }
+    }
+}
