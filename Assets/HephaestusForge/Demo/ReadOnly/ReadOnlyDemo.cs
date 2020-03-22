a8f098aeca8120d21be225a5e692aea1b5e36838
MathDodo <mathias.h.larsen@jp.dk>
2020-03-22 12:59:40 +0100

    Minor change

--- Assets/HephaestusForge/Demo/ReadOnly/ReadOnlyDemo.cs
+++ Assets/HephaestusForge/Demo/ReadOnly/ReadOnlyDemo.cs
@@ -13,7 +13,7 @@ namespace HephaestusForge.ReadOnly
         private int _test1;
 
         [SerializeField, ReadOnly]
-        private int _test2 = 445;
+        private int _test2;
 
 #pragma warning restore 0649
     }
